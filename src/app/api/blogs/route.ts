8a0db1f4ceb0631124660d10f75036d53f03e9c1
Babak Taghizadeh <babak.tz98@gmail.com>
2024-10-04 22:21:12 +0330

    data directory fixed

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 import { BlogProps } from "@/types/types";
+import { join } from 'path';
 
 const getBlogs = async(): Promise<BlogProps[]> => {
-    const jsonData = await fs.readFile(process.cwd() + '/api/blogs/blog.json', 'utf-8');
+    const jsonData = await fs.readFile(process.cwd() + '/src/app/api/blogs/blog.json', 'utf-8');
     return JSON.parse(jsonData);
 } 
 
