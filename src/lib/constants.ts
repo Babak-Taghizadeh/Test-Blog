13de4b493a798b42f487483dc616555eab19e28c
Babak Taghizadeh <babak.tz98@gmail.com>
2024-10-16 22:29:10 +0330

    new url added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const API_ROUTES = {
-    LOGIN_REQUEST: "https://test-blog10.vercel.app/api/auth",
-    BLOGS_FETCH: "https://test-blog10.vercel.app/api/data",
-    BLOG_FETCH: (id: string) => `https://test-blog10.vercel.app/api/data/${id}`
+    LOGIN_REQUEST: "https://dummy-blog10.vercel.app/api/auth",
+    BLOGS_FETCH: "https://dummy-blog10.vercel.app/api/data",
+    BLOG_FETCH: (id: string) => `https://dummy-blog10.vercel.app/api/data/${id}`
 }
 
 export const ROUTES = {
